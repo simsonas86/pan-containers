88d0fbc303d20c26a08e6048aff2b625323984ae
Simon <simsonas86@gmail.com>
2024-09-12 01:03:44 +0300

    chore(SQL): clean-up

--- containers.sql
+++ containers.sql
@@ -1,13 +1,10 @@
-CREATE TABLE `pan_containers` (
+CREATE TABLE IF NOT EXISTS `pan_containers` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`label` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`coords` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`heading` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
-	`target` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb3_general_ci',
+	`label` LONGTEXT NULL DEFAULT NULL,
+	`coords` LONGTEXT NULL DEFAULT NULL,
+	`heading` LONGTEXT NULL DEFAULT NULL,
+	`target` LONGTEXT NULL DEFAULT NULL,
 	`lastupdated` TIMESTAMP NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	`isdeleted` TINYTEXT NOT NULL DEFAULT 'false' COLLATE 'utf8mb3_general_ci',
+	`isdeleted` TINYTEXT NOT NULL DEFAULT 'false',
 	PRIMARY KEY (`id`) USING BTREE
-)
-COLLATE='utf8mb3_general_ci'
-ENGINE=InnoDB
-;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8mb3_general_ci;
